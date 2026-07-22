c07883206597c614f2dbe045830b0d8ea435ee1f
denizberkin <berkindeniz2000@gmail.com>
2026-07-22 12:25:32 +0300

    torch checkpointing

--- eval_pipeline/components/models/__init__.py
+++ eval_pipeline/components/models/__init__.py
@@ -1,4 +1,5 @@
 from eval_pipeline.components.models.base import ModelFactory
 from eval_pipeline.components.models.build import build_model_factory
+from eval_pipeline.components.models.torch_checkpoint import load_torch_checkpoint, save_torch_checkpoint
 
-__all__ = ["ModelFactory", "build_model_factory"]
+__all__ = ["ModelFactory", "build_model_factory", "load_torch_checkpoint", "save_torch_checkpoint"]
